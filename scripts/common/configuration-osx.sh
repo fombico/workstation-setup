7645f9584fe1d8b20dac159f39ae25de6c64bf3b
Fredrich Ombico <fombico@pivotal.io>
2019-03-20 13:14:52 -0400

    Add some OS X settings

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -13,6 +13,8 @@ killall SystemUIServer
 # fast key repeat rate, requires reboot to take effect
 defaults write ~/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
 defaults write ~/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
+defaults write ~/Library/Preferences/.GlobalPreferences AppleShowScrollBars -string "Always"
+defaults write ~/Library/Preferences/.GlobalPreferences AppleScrollerPagingBehavior -int 1
 
 # set finder to display full path in title bar
 defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
