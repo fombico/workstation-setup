f3117406b58578a0665dad0b68e4c72f29b9627f
Fredrich Ombico <fombico@pivotal.io>
2019-03-20 10:42:35 -0400

    Add some iTerm color themes

--- scripts/common/finished.sh
+++ scripts/common/finished.sh
@@ -24,4 +24,7 @@ echo
 echo "After checking the above output for any problems, start a new iTerm session to make use of all the installed tools."
 echo "Rebooting is only necessary for keyboard repeat settings to work."
 
+echo ""
+echo "Install iTerm themes from the files/"
+
 echo
