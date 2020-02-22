2b3cfdf6150653d88807d1a5ffd43e7d549a7898
Fredrich Ombico <fombico@pivotal.io>
2020-02-22 00:48:11 -0500

    Add git tab completion to zsh config

--- scripts/common/configuration-zsh.sh
+++ scripts/common/configuration-zsh.sh
@@ -10,3 +10,19 @@ cp ../../files/fombico.zsh-theme ~/.oh-my-zsh/themes
 
 echo "Setting theme"
 sed -ie 's/ZSH_THEME=".*"/ZSH_THEME="fombico"/g' ~/.zshrc
+
+echo "Setting up git tab completion"
+mkdir -p ~/.zsh
+cd ~/.zsh
+
+# Download the scripts
+curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+
+echo -e "
+# Load Git tab completion
+zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash
+fpath=(~/.zsh $fpath)
+
+autoload -Uz compinit && compinit
+" >> ~/.zshrc
